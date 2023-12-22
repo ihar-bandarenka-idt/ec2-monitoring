5e72d2d8c94f49f0234fd3c6631e831f06363601
ibandarenka <ihar.bandarenka@idt.net>
2023-12-22 16:16:02 +0300

    ec2 monitoring

--- app.py
+++ app.py
@@ -7,22 +7,7 @@ from ec2_monitoring.ec2_monitoring_stack import Ec2MonitoringStack
 
 
 app = cdk.App()
-Ec2MonitoringStack(app, "Ec2MonitoringStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+env = cdk.Environment(region="eu-central-1", account="542258342574")
+Ec2MonitoringStack(app, "Ec2MonitoringStack", env=env)
 
 app.synth()
--- ec2_monitoring/ec2_monitoring_stack.py
+++ ec2_monitoring/ec2_monitoring_stack.py
@@ -1,19 +1,41 @@
+import os.path
+
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2, Stack, aws_iam as iam
+
 )
+
 from constructs import Construct
+with open(os.path.join("ec2_monitoring", "user_data.sh")) as f:
+    user_data = f.read()
+print(user_data)
 
 class Ec2MonitoringStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        role = iam.Role(self, "instance-role", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+                             managed_policies=[
+                                 iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchAgentServerPolicy")])
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "Ec2MonitoringQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+        )
+        self.vpc = ec2.Vpc.from_lookup(
+            self, "VPC"
+            , vpc_id = "vpc-e8a9dc83"
+        )
+
+
+        instance = ec2.Instance(self, "Instance",
+                                instance_type=ec2.InstanceType("t3.small"),
+                                machine_image=amzn_linux,
+                                vpc=self.vpc,
+                                user_data=ec2.UserData.custom(user_data),
+                                role=role,
+                                key_name="test"
+                                )
